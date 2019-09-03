9adf8728ee38e2743220d17670674b8a29710b0a
MrGio7 <lashagiorgi1233@gmail.com>
2019-09-03 12:27:59 +0400

    added loginHandler

--- api/Users.js
+++ api/Users.js
@@ -22,9 +22,42 @@ router.post("/register", (req, res) => {
       }
     })
     .catch(err => {
-      console.log(err);
       res.status(500).json({ message: `cant connect to db error: ${err}` });
     });
 });
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  db("users")
+    .where({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res
+          .status(200)
+          .json({ message: `welcome user ${user.username}`, token });
+      } else {
+        res.status(401).json("invlid credentials");
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    id: user.id,
+    user: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
